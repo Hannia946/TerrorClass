7705b0b6f38c68be7a42e550fbf041772066a84f
Hannia <a23110329@ceti.mx>
2024-12-06 23:58:16 -0600

    Actualización de contenido

---
+++ Styles/archivo1.js
@@ -0,0 +1,18 @@
+﻿const formulario = document.getElementById('formulario');
+const passwordInput = document.getElementById('contraseña');
+const mensaje = document.getElementById('mensaje');
+
+formulario.addEventListener('submit', function (event) {
+    if (passwordInput.value.length < 8) {
+        event.preventDefault();
+        mensaje.style.display = 'block';
+    } else {
+        mensaje.style.display = 'none';
+    }
+})
+
+const titulo = document.getElementById('bienvenida'); 
+
+setInterval(() => {
+    titulo.style.visibility = titulo.style.visibility === 'hidden' ? 'visible' : 'hidden';
+}, 500);
